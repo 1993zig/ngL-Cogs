bf2f2fdfd98b41d9914c8c15b754251a09c58dda
einsatzbereit <niklas.goetzen@gmail.com>
2023-05-06 18:07:55 +0200

    typo in import

--- emotemanager/__init__.py
+++ emotemanager/__init__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from redbot.core.bot import Red
 
-from .zcg import EmoteManager
+from .emotemanager import EmoteManager
 
 with open(Path(__file__).parent / "info.json", encoding="utf8") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
